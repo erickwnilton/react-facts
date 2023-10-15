8ea1bb8a248d9817eb627b13afaff2df343950b9
23eerick <erickwelberth1@hotmail.com>
2023-10-15 09:16:21 -0300

    create app main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,15 @@
+import styles from "../styles/home.module.css";
+
 export default function Main() {
   return (
-    <h1>app</h1>
+    <div className={styles.container}>
+      <div className={styles.child}>
+        <div className={styles.second_child}>
+          <button className={styles.button}>
+            &#9889;
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
